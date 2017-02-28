924a91e3c4827066f47cf547d25473a7a391fd58
amin007 <helmi007@yahoo.com>
2017-02-28 15:33:02 +0800

    masukkan sample invois

---
+++ sumber/fail/teks/sample-invois.php
@@ -0,0 +1,36 @@
+My Awesome Company Name
+200 Jalan Lurus, Taman Bunga Orchid, 53300 Kuala Lumpur – Tel: 012-222 4455
+
+	INVOICE
+ACME Sdn. Bhd.
+100-10 Bangunan Terkemuka,
+54321 Kuala Lumpur, Malaysia
+Attn: Ahmad Kasan	
+	Ref:	RUJUKAN-2014-01(1/3)
+	Date:	March 10, 2014
+
+Dear Sir,
+
+Please find the invoice below for the services rendered:
+
+| Items	                                    | Price (RM) | Qty   | Total (RM) |
+| Custom Wordpress Plugin Development (1/3) |
+| First payment of 10% – project kick-off	| XXXXXX	 | 1	 | XXXXXX     |
+                                                         | TOTAL | XXXXXX     |
+
+Cheque or direct bank deposit. Please notify us if you made any direct bank deposits. Payments can be made to:
+
+MY AWESOME COMPANY NAME
+CIMB BANK
+A/C NO.: 8000522622
+
+Any questions or inquiries can be directed to Khairil Iszuddin at the number 012-222 4455
+
+Regards,
+
+
+Khairil Iszuddin Ismail
+Web Programmer
+MY AWESOME COMPANY NAME
+
+This document is left unsigned intentionally as it is created and delivered via online means.
