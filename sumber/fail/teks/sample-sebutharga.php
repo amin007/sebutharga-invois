8c990f7414d7727c15be151d01cc1acf43bf9439
amin007 <helmi007@yahoo.com>
2017-02-28 15:31:47 +0800

    Create sample-sebutharga.php

---
+++ sumber/fail/teks/sample-sebutharga.php
@@ -0,0 +1,69 @@
+My Awesome Company Name
+200 Jalan Lurus, Taman Bunga Orchid, 53300 Kuala Lumpur – Tel: 012-222 4455
+
+	QUOTE
+ACME Sdn. Bhd.
+100-10 Bangunan Terkemuka,
+54321 Kuala Lumpur, Malaysia
+Attn: Ahmad Kasan	
+	Ref:	RUJUKAN-2014-01
+	Date:	March 3, 2014
+
+Dear Sir,
+
+Please find our quotation for the inquired products and services below:
+
+| Items	                                 | Price (RM) | Qty   | Total (RM) |
+| Custom Wordpress Plugin Development
+| To create an awesome wordpress plugin 
+| to do marvelous things on your website | XXXXXX	  | 1	  | XXXXXX     |
+                                                      | TOTAL | XXXXXX     |
+
+
+Project Scope
+To create an awesome wordpress plugin to do marvelous things on your website
+
+Features To Include
+•	Wordpress plugin is able to produce candy from thin air
+•	Option panel to allow admin to choose candy size, flavour and colour of the candy
+•	Option panel to schedule candy making
+•	Option to save candy creation configuration
+
+Payment Terms
+Payment Terms:
+–	20% payment is required upon project kick-off.
+–	Another 30% payment is required after first project demo.
+–	Balance to be paid before full delivery of codes.
+
+Payment Method
+Cheque or direct bank deposit. Please notify us if you made any direct bank deposits. Payments can be made to:
+
+MY AWESOME COMPANY NAME
+CIMB BANK
+A/C NO.: 8000522622
+
+Validity
+This quote is valid for 14 days from the date of this document.
+
+
+Warranty
+30-day warranty is provided to ensure that website is working properly and bug-free. Any bug fix requests reported after the 30-day period is chargeable. This does not include development of new features and enhancements.
+
+Ownership
+All works will be the ownership of the client except for third-party works used in the project such as and not limited to open source libraries, artworks, commercial libraries which comes with their own license agreement. Client is to abide by the license terms and hold full responsibility for using them.
+
+Other Terms
+This quote does not include charges such as acquiring stock photos, acquiring fonts, professional photography, pre- and post-video production, talents and others. Other charges, like those will be quoted separately upon request. Price quoted is subject to change based on client’s requests of other services and additional features.
+
+We trust that you will find the quote above satisfactory. We look forward to working with you. Please contact us should have any questions at all.
+
+Any questions or inquiries can be directed to Khairil Iszuddin at the number 012-222 4455
+
+Regards,
+
+
+Khairil Iszuddin Ismail
+Web Programmer
+MY AWESOME COMPANY NAME
+
+This document is left unsigned intentionally as it is created and delivered via online means.
