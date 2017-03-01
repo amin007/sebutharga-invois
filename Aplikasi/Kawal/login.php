098c63b4a89ab492861f0452a438048be6fca284
amin007 <helmi007@yahoo.com>
2017-03-01 12:46:36 +0800

    tambah fail login

---
+++ Aplikasi/Kawal/login.php
@@ -0,0 +1,64 @@
+<?php
+namespace Aplikasi\Kawal; //echo __NAMESPACE__; 
+class Login extends \Aplikasi\Kitab\Kawal
+{
+#==========================================================================================
+	function __construct() 
+	{
+		//echo '<br>class Index extends Kawal';
+		parent::__construct();
+		\Aplikasi\Kitab\Kebenaran::kawalMasuk();
+		$this->_folder = 'login';
+	}
+#==========================================================================================
+	public function index()
+	{
+		# Set pemboleubah utama
+		$this->papar->menuatas = 'tak';
+		$this->papar->TajukBesar = 'Sila Login';
+		
+		# pergi papar kandungan
+		$jenis = $this->papar->pilihTemplate($template=0);
+		$this->papar->bacaTemplate(
+		//$this->papar->paparTemplate(
+		'login/index',$jenis,0); # $noInclude=0
+	}
+#==========================================================================================	
+	function semakid2()
+	{
+		# debug $_POST
+		echo '<pre>Test $_POST->'; print_r($_POST) . '</pre>';
+		//$this->tanya->dapatid($_POST['password']);
+		
+		# semak data $this->tanya->ujiID();
+		//$this->tanya->semakid();
+
+	}
+
+	function salah()
+	{
+		# debug
+		//$this->tanya->dapatid($_POST['password']);
+		$this->papar->mesej = 'Ada masalah pada user dan password';
+
+		# Set pemboleubah utama
+		$this->papar->sesat = 'Enjin Carian Ekonomi - Sesat';
+		$this->papar->isi = '';
+
+		# pergi papar kandungan
+		$jenis = $this->papar->pilihTemplate($template=0);
+		$this->papar->bacaTemplate(
+		//$this->papar->paparTemplate(
+			'index/salah',$jenis,0); # $noInclude=0
+		
+	}
+#==========================================================================================	
+	function semaknama($nama)
+	{
+		# semak data $_POST
+		//echo '<pre>Test $_POST->'; print_r($_POST) . '</pre>';
+		$this->tanya->dapatid($nama);
+		//$this->tanya->ujiID();
+	}
+#==========================================================================================	
+}
