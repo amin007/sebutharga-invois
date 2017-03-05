9a26d75a8a4b467a957a69262671feea4ca1ea59
amin007 <helmi007@yahoo.com>
2017-03-05 18:27:16 +0800

    tambah fungsi tukartarikh dan pembolehubah lain

--- Aplikasi/Papar/akaun/cetak.php
+++ Aplikasi/Papar/akaun/cetak.php
@@ -1,4 +1,11 @@
 <?php 
+function tukartarikh($lama)
+{
+	$baru1 = @date_format($lama, 'j F, Y, g:i a');
+	$baru2 = date("j F, Y, g:i a");	
+	$baru = ($lama == '0000-00-00 00:00:00') ? $baru2 : $baru1;
+	return $baru;
+}
 include 'diatas.php';
 /*echo '<pre>';
 echo '$this->akaun:<br>'; print_r($this->akaun); 
@@ -17,16 +24,6 @@ else: # set pembolehubah
 	$mesej = '::' . $this->cariID . ' tiada dalam ' . $this->_jadual;
 endif;
 //echo '<div align="center">' . $mencari . '</div>'; ?><?php 
-/*
-$this->akaun['kes'][0]['id'] 
-$this->akaun['kes'][0]['Nama'] 
-$this->akaun['kes'][0]['Tajuk']
-$this->akaun['kes'][0]['Mesej'] 
-$this->akaun['kes'][0]['Email'] 
-$this->akaun['kes'][0]['Bayaran']
-$this->akaun['kes'][0]['Status'] 
-$this->akaun['kes'][0]['Temujanji']	
-*/
 
 if ($this->carian=='[tiada id diisi]')
     echo 'data kosong<br>';
@@ -34,43 +31,64 @@ elseif(!isset($this->akaun['kes'][0]['id']))
 	echo 'data kosong juga<br>';
 else # $this->carian=='ada' - mula 
 {
-	/*$namaSyarikat = '';
+	/*$namaOrang = '';
+	$namaSyarikat = '';
 	$alamat = '';
 	$notel = '';
 	$namaBank = '';
-	$namaAkaunBank = '';
-	//*/
+	$namaAkaunBank = ''; //*/
+
+	$namaOrang = 'Khairil Iszuddin Ismail';
 	$namaSyarikat = 'My Awesome Company Name';
-	$alamat = '200 Jalan Lurus, Taman Bunga Orchid, 53300 Kuala Lumpur';
+	$alamat = '200 Jalan Lurus,<br> Taman Bunga Orchid,<br> 53300 Kuala Lumpur';
 	$notel = '012-222 4455';
 	$namaBank = 'CIMB BANK';
-	$namaAkaunBank = '8000522622';
+	$namaAkaunBank = '8000522622';//*/
+
+	$kira = count($this->akaun['kes']);
+	for($i = 1; $i <= $kira; $i++):
+		$tarikh = tukartarikh($this->akaun['kes'][$i]['Tarikh']);
+		/*
+		$this->akaun['kes'][0]['id'] 
+		$this->akaun['kes'][0]['Nama'] 
+		$this->akaun['kes'][0]['Tajuk']
+		$this->akaun['kes'][0]['Mesej'] 
+		$this->akaun['kes'][0]['Email'] 
+		$this->akaun['kes'][0]['Bayaran']
+		$this->akaun['kes'][0]['Status'] 
+		$this->akaun['kes'][0]['Temujanji']	//*/
 ?>
-<div class="container">
+<div class="container" style="page-break-before:always">
 <div class="jumbotron">
 
-	<h4><?php echo $namaSyarikat ?></h4>
-	<h4><?php echo $alamat . ' - Tel: ' . $notel ?></h4>
-	<h1 align="right">INVOICE</h1>
-
+	<div class="row">
+		<div class="col-md-9">	
+			<h4><?php echo $namaSyarikat ?></h4>
+			<h4><?php echo $alamat . ' - Tel: ' . $notel ?></h4>
+		</div>
+		<div class="col-md-3">
+			<div class="teksBesar">INVOICE - <span class="text-danger">PAID</span></div>
+		</div>
+	</div>
 	<ul>
-	<li><?php echo $this->akaun['kes'][0]['Nama'] ?></li>
-	<li><?php echo $this->akaun['kes'][0]['Email'] ?></li>
-	<li><?php echo $this->akaun['kes'][0]['Temujanji'] ?></li>
+	<li><?php echo $this->akaun['kes'][$i]['Nama'] ?></li>
+	<li><?php echo $this->akaun['kes'][$i]['Email'] ?></li>
+	<li>Note : <?php echo $this->akaun['kes'][$i]['Temujanji'] ?></li>
 	<!-- li>100-10 Bangunan Terkemuka,</li -->
 	<!-- li>54321 Kuala Lumpur, Malaysia</li -->
 	<!-- li>Attn: Ahmad Kasan</li -->
-	<li>Ref:	RUJUKAN-2014-01(1/3)</li>
-	<li>Date:	March 10, 2014</li>
+	<!-- li>Ref:	RUJUKAN-2014-01(1/3)</li -->
+	<!-- li>Date:	March 10, 2014</li -->
+	<!-- li>Date: <?php //echo $tarikh ?></li -->
 	</ul>
 
 	<h4>Please find the invoice below for the services rendered:</h4><hr>
 
 	<div class="row">
 		<div class="col-md-2"> <strong>Subjek :</strong> <?php 
-			echo nl2br($this->akaun['kes'][0]['Tajuk']); ?></div>
+			echo nl2br($this->akaun['kes'][$i]['Tajuk']); ?></div>
 		<div class="col-md-10"> <strong>Mesej  :</strong> <?php 
-			echo nl2br($this->akaun['kes'][0]['Mesej']); ?></div>
+			//echo nl2br($this->akaun['kes'][$i]['Mesej']); ?></div>
 	</div><hr>
 	<table class="table table-condensed">
 	<!-- table class="excel" -->
@@ -85,10 +103,6 @@ else # $this->carian=='ada' - mula
 	</tr>
 	</table><hr>
 
-<?php
-} # $this->carian=='ada' - tamat 
-//*/
-?>
 	<h4>Cheque or direct bank deposit. <br> 
 	Please notify us if you made any direct bank deposits. <br>
 	Payments can be made to:</h4>
@@ -98,16 +112,23 @@ else # $this->carian=='ada' - mula
 	<li> A/C NO.: <?php echo $namaAkaunBank ?> </li>
 	</ul>
 
-	<h4>Any questions or inquiries can be directed to Khairil Iszuddin at the number <?php echo $notel ?></h4>
+	<h4>Any questions or inquiries can be directed to <?php echo $namaOrang ?> at the number <?php echo $notel ?></h4>
 	<h4> Regards, </h4>
 
 	<ul>
-	<li> Khairil Iszuddin Ismail </li>
+	<li> <?php echo $namaOrang ?> </li>
 	<li> Web Programmer </li>
 	<li> <?php echo $namaSyarikat ?> </li>
 	</ul>
 
 </div><!-- / class="jumbotron" -->
 </div><!-- / class="container" -->
+
 <?php
+	endfor; # endfor
+} # $this->carian=='ada' - tamat 
+//*/
+?>
+<?php
+
 include 'dibawah.php';
