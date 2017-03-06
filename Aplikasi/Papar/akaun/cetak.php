902e8e99eabe23c931ed91e2433d4c3603386c70
amin007 <helmi007@yahoo.com>
2017-03-06 14:31:56 +0800

    kemaskini koding cetak.php
    1. tambah fungsi pecahTarikhMesej
    2. tambah tajuk "Khas Untuk Paparan Pihak Bank Sahaja"
    3. wujukan $tarikh, $dataMesej, $bilRujukan dan $email 
    4. tukar Qty kepada Day

--- Aplikasi/Papar/akaun/cetak.php
+++ Aplikasi/Papar/akaun/cetak.php
@@ -4,14 +4,36 @@ function tukartarikh($lama)
 	$baru1 = @date_format($lama, 'j F, Y, g:i a');
 	$baru2 = date("j F, Y, g:i a");	
 	$baru = ($lama == '0000-00-00 00:00:00') ? $baru2 : $baru1;
+
 	return $baru;
 }
+function pecahTarikhMesej($mesej)
+{
+	@list($dataAsal, $data) = explode('Tarikh',$mesej);
+	@list($tarikh, $data2) = explode('Masa',$data);
+	@list($dataPC, $data3) = explode('Mesej',$data2);
+	$data4 = (isset($data3)) ? $data3 : '';
+	@list($dataMesej, $dataAkhir) = explode('-',$data4);
+	$dataMesej = (isset($dataMesej)) ? $dataMesej : '';
+	
+	return array($tarikh, $dataMesej);
+}
 include 'diatas.php';
+?><h1 align="center">Khas Untuk Paparan Pihak Bank Sahaja</h1><?php
 /*echo '<pre>';
 echo '$this->akaun:<br>'; print_r($this->akaun); 
 echo '<br>$this->carian:'; print_r($this->carian); 
 echo '<br>$this->cariID:'; print_r($this->cariID); 
 echo '</pre>'; //*/
+		/* --
+		$this->akaun['kes'][0]['id'] 
+		$this->akaun['kes'][0]['Nama'] 
+		$this->akaun['kes'][0]['Tajuk']
+		$this->akaun['kes'][0]['Mesej'] 
+		$this->akaun['kes'][0]['Email'] 
+		$this->akaun['kes'][0]['Bayaran']
+		$this->akaun['kes'][0]['Status'] 
+		$this->akaun['kes'][0]['Temujanji']	//*/
 
 if(isset($this->akaun['kes'][0]['id'])):
 	# set pembolehubah
@@ -46,17 +68,20 @@ else # $this->carian=='ada' - mula
 	$namaAkaunBank = '8000522622';//*/
 
 	$kira = count($this->akaun['kes']);
-	for($i = 1; $i <= $kira; $i++):
-		$tarikh = tukartarikh($this->akaun['kes'][$i]['Tarikh']);
-		/*
-		$this->akaun['kes'][0]['id'] 
-		$this->akaun['kes'][0]['Nama'] 
-		$this->akaun['kes'][0]['Tajuk']
-		$this->akaun['kes'][0]['Mesej'] 
-		$this->akaun['kes'][0]['Email'] 
-		$this->akaun['kes'][0]['Bayaran']
-		$this->akaun['kes'][0]['Status'] 
-		$this->akaun['kes'][0]['Temujanji']	//*/
+	for($i = 0; $i < $kira; $i++):
+		//@$tarikh = tukartarikh($this->akaun['kes'][$i]['Tarikh']);
+		list($tarikh,$dataMesej) = pecahTarikhMesej($this->akaun['kes'][$i]['Mesej']);
+		# untuk semakan ID
+		$bilRujukan =  \Aplikasi\Kitab\RahsiaHash::rahsia('md5', $dataMesej);
+		$id = $this->akaun['kes'][0]['id'];
+		$bilRujukan = $bilRujukan . '@'. $i . '@' . $id . '/' .$kira;
+		# untuk semakan email
+		$email = ($this->akaun['kes'][$i]['Email']);
+		$email = (isset($email)) ? $email : '';
+		/*echo '<pre>jumlah data = ' . $kira . '</pre>';
+		echo '<pre>$tarikh '; print_r($tarikh); echo '</pre>';
+		//echo '<pre>$dataEmail:'; print_r($dataEmail); echo '</pre>';
+		echo '<pre>$dataMesej:'; print_r($dataMesej); echo '</pre>';//*/
 ?>
 <div class="container" style="page-break-before:always">
 <div class="jumbotron">
@@ -74,12 +99,13 @@ else # $this->carian=='ada' - mula
 	<li><?php echo $this->akaun['kes'][$i]['Nama'] ?></li>
 	<li><?php echo $this->akaun['kes'][$i]['Email'] ?></li>
 	<li>Note : <?php echo $this->akaun['kes'][$i]['Temujanji'] ?></li>
+	<li>Bil Rujukan : <?php echo $bilRujukan ?>/PAID</li>
 	<!-- li>100-10 Bangunan Terkemuka,</li -->
 	<!-- li>54321 Kuala Lumpur, Malaysia</li -->
 	<!-- li>Attn: Ahmad Kasan</li -->
 	<!-- li>Ref:	RUJUKAN-2014-01(1/3)</li -->
 	<!-- li>Date:	March 10, 2014</li -->
-	<!-- li>Date: <?php //echo $tarikh ?></li -->
+	<li>Date <?php echo $tarikh ?></li>
 	</ul>
 
 	<h4>Please find the invoice below for the services rendered:</h4><hr>
@@ -87,13 +113,14 @@ else # $this->carian=='ada' - mula
 	<div class="row">
 		<div class="col-md-2"> <strong>Subjek :</strong> <?php 
 			echo nl2br($this->akaun['kes'][$i]['Tajuk']); ?></div>
-		<div class="col-md-10"> <strong>Mesej  :</strong> <?php 
-			//echo nl2br($this->akaun['kes'][$i]['Mesej']); ?></div>
+		<div class="col-md-10"> <strong>Kandungan Mesej  :</strong> <?php 
+			//echo nl2br($dataE); 
+			echo nl2br($dataMesej); ?></div>
 	</div><hr>
 	<table class="table table-condensed">
 	<!-- table class="excel" -->
 	<tr>
-		<td> Items </td><td> Price (RM) </td><td> Qty </td><td> Total (RM) </td>
+		<td> Items </td><td> Price (RM) </td><td> Day </td><td> Total (RM) </td>
 	</tr>
 	<tr>
 		<td> 1 </td><td> 500 </td><td> 1 </td><td> 500 </td>
