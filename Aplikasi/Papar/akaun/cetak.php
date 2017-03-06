25fcd4175bcc6f61fa75631b3c21de58fb560037
amin007 <helmi007@yahoo.com>
2017-03-06 16:10:27 +0800

    ubah koding cetak.php
    1. tukar Masa kepada Takwim
    2. buang koding if(isset($this->akaun['kes'][0]['id'])): dan space tab
    3. tukar $kira kepada $kiraPesanan 
    4. tambah nl2br() pada $tarikh
    5. <tr><td> dalam sebaris sahaja

--- Aplikasi/Papar/akaun/cetak.php
+++ Aplikasi/Papar/akaun/cetak.php
@@ -10,84 +10,71 @@ function tukartarikh($lama)
 function pecahTarikhMesej($mesej)
 {
 	@list($dataAsal, $data) = explode('Tarikh',$mesej);
-	@list($tarikh, $data2) = explode('Masa',$data);
+	//@list($tarikh, $data2) = explode('Masa',$data);
+	@list($tarikh, $data2) = explode('Takwim',$data);
 	@list($dataPC, $data3) = explode('Mesej',$data2);
 	$data4 = (isset($data3)) ? $data3 : '';
 	@list($dataMesej, $dataAkhir) = explode('-',$data4);
 	$dataMesej = (isset($dataMesej)) ? $dataMesej : '';
-	
+
 	return array($tarikh, $dataMesej);
 }
 include 'diatas.php';
 ?><h1 align="center">Khas Untuk Paparan Pihak Bank Sahaja</h1><?php
 /*echo '<pre>';
-echo '$this->akaun:<br>'; print_r($this->akaun); 
-echo '<br>$this->carian:'; print_r($this->carian); 
-echo '<br>$this->cariID:'; print_r($this->cariID); 
+echo '$this->akaun:<br>'; print_r($this->akaun);
+echo '<br>$this->carian:'; print_r($this->carian);
+echo '<br>$this->cariID:'; print_r($this->cariID);
 echo '</pre>'; //*/
 		/* --
-		$this->akaun['kes'][0]['id'] 
-		$this->akaun['kes'][0]['Nama'] 
+		$this->akaun['kes'][0]['id']
+		$this->akaun['kes'][0]['Nama']
 		$this->akaun['kes'][0]['Tajuk']
-		$this->akaun['kes'][0]['Mesej'] 
-		$this->akaun['kes'][0]['Email'] 
+		$this->akaun['kes'][0]['Mesej']
+		$this->akaun['kes'][0]['Email']
 		$this->akaun['kes'][0]['Bayaran']
-		$this->akaun['kes'][0]['Status'] 
+		$this->akaun['kes'][0]['Status']
 		$this->akaun['kes'][0]['Temujanji']	//*/
-
-if(isset($this->akaun['kes'][0]['id'])):
-	# set pembolehubah
-	$mencari = URL . 'akaun/ubahCari/';
-	$carian = $this->cariID;
-	$mesej = ''; //$carian .' ada dalam ' . $this->_jadual;
-else: # set pembolehubah
-	$mencari = URL . 'akaun/ubahCari/';
-	$carian = null;
-	$mesej = '::' . $this->cariID . ' tiada dalam ' . $this->_jadual;
-endif;
-//echo '<div align="center">' . $mencari . '</div>'; ?><?php 
 
 if ($this->carian=='[tiada id diisi]')
     echo 'data kosong<br>';
 elseif(!isset($this->akaun['kes'][0]['id']))
 	echo 'data kosong juga<br>';
-else # $this->carian=='ada' - mula 
+else # $this->carian=='ada' - mula
 {
 	/*$namaOrang = '';
 	$namaSyarikat = '';
 	$alamat = '';
 	$notel = '';
 	$namaBank = '';
-	$namaAkaunBank = ''; //*/
+	$namaAkaunBank = '';//*/
 
-	$namaOrang = 'Khairil Iszuddin Ismail';
+	$namaOrang = 'Khairil Iszuddin Ismail'; # contoh sahaja
 	$namaSyarikat = 'My Awesome Company Name';
 	$alamat = '200 Jalan Lurus,<br> Taman Bunga Orchid,<br> 53300 Kuala Lumpur';
 	$notel = '012-222 4455';
 	$namaBank = 'CIMB BANK';
 	$namaAkaunBank = '8000522622';//*/
 
-	$kira = count($this->akaun['kes']);
+	$kiraPesanan = count($this->akaun['kes']);
 	for($i = 0; $i < $kira; $i++):
-		//@$tarikh = tukartarikh($this->akaun['kes'][$i]['Tarikh']);
 		list($tarikh,$dataMesej) = pecahTarikhMesej($this->akaun['kes'][$i]['Mesej']);
 		# untuk semakan ID
 		$bilRujukan =  \Aplikasi\Kitab\RahsiaHash::rahsia('md5', $dataMesej);
 		$id = $this->akaun['kes'][0]['id'];
-		$bilRujukan = $bilRujukan . '@'. $i . '@' . $id . '/' .$kira;
+		$bilRujukan = $bilRujukan . '@'. $i . '@' . $id . '/' . $kiraPesanan;
 		# untuk semakan email
 		$email = ($this->akaun['kes'][$i]['Email']);
 		$email = (isset($email)) ? $email : '';
-		/*echo '<pre>jumlah data = ' . $kira . '</pre>';
+		/*echo '<pre>jumlah data = ' . $kira . '</pre>'; # debug data
 		echo '<pre>$tarikh '; print_r($tarikh); echo '</pre>';
-		//echo '<pre>$dataEmail:'; print_r($dataEmail); echo '</pre>';
 		echo '<pre>$dataMesej:'; print_r($dataMesej); echo '</pre>';//*/
 ?>
 <div class="container" style="page-break-before:always">
 <div class="jumbotron">
 
 	<div class="row">
-		<div class="col-md-9">	
+		<div class="col-md-9">
 			<h4><?php echo $namaSyarikat ?></h4>
 			<h4><?php echo $alamat . ' - Tel: ' . $notel ?></h4>
 		</div>
@@ -105,7 +92,7 @@ else # $this->carian=='ada' - mula
 	<!-- li>Attn: Ahmad Kasan</li -->
 	<!-- li>Ref:	RUJUKAN-2014-01(1/3)</li -->
 	<!-- li>Date:	March 10, 2014</li -->
-	<li>Date <?php echo $tarikh ?></li>
+	<li>Date Email <?php echo nl2br($tarikh) ?></li>
 	</ul>
 
 	<h4>Please find the invoice below for the services rendered:</h4><hr>
@@ -113,21 +100,14 @@ else # $this->carian=='ada' - mula
 	<div class="row">
 		<div class="col-md-2"> <strong>Subjek :</strong> <?php 
 			echo nl2br($this->akaun['kes'][$i]['Tajuk']); ?></div>
-		<div class="col-md-10"> <strong>Kandungan Mesej  :</strong> <?php 
-			//echo nl2br($dataE); 
+		<div class="col-md-10"> <strong>Kandungan Mesej  :</strong> <?php
 			echo nl2br($dataMesej); ?></div>
 	</div><hr>
 	<table class="table table-condensed">
 	<!-- table class="excel" -->
-	<tr>
-		<td> Items </td><td> Price (RM) </td><td> Day </td><td> Total (RM) </td>
-	</tr>
-	<tr>
-		<td> 1 </td><td> 500 </td><td> 1 </td><td> 500 </td>
-	</tr>
-	<tr>
-		<td colspan="2">&nbsp;</td><td> All Total (RM) </td><td> 500 </td>
-	</tr>
+	<tr><td> Items </td><td> Price (RM) </td><td> Day </td><td> Total (RM) </td></tr>
+	<tr><td> 1 </td><td> 500 </td><td> 1 </td><td> 500 </td></tr>
+	<tr><td colspan="2">&nbsp;</td><td> All Total (RM) </td><td> 500 </td></tr>
 	</table><hr>
 
 	<h4>Cheque or direct bank deposit. <br> 
@@ -155,7 +135,5 @@ else # $this->carian=='ada' - mula
 	endfor; # endfor
 } # $this->carian=='ada' - tamat 
 //*/
-?>
-<?php
 
 include 'dibawah.php';
