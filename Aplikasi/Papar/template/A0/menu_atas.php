6047ea3d195c7b73cb96b9480bc3a28e1233a4f1
amin007 <helmi007@yahoo.com>
2017-03-07 15:53:34 +0800

    tukar pembolehubah

--- Aplikasi/Papar/template/A0/menu_atas.php
+++ Aplikasi/Papar/template/A0/menu_atas.php
@@ -2,17 +2,17 @@
 $sesi = \Aplikasi\Kitab\Sesi::init();
 //echo '<pre>MENU_ATAS - $_SESSION:', print_r($_SESSION, 1) . '</pre><br>';
 # set pembolehubah
-$pengguna = \Aplikasi\Kitab\Sesi::get('namaPegawai');
-$level = \Aplikasi\Kitab\Sesi::get('levelPegawai');
+$pengguna = \Aplikasi\Kitab\Sesi::get('namaPendek');
+$level = \Aplikasi\Kitab\Sesi::get('levelPengguna');
 
-$senaraiPengguna = array('fe','pegawai','pegawaiprosesan', 'feprosesan');
-$senaraiPentadbir = array('kawal','admin');
+$senaraiPengguna = array('pentadbir','biasa');
+$senaraiPentadbir = array('pentadbir','biasa');
 if (in_array($level, $senaraiPentadbir)) 
 	$paras = '' . $level;
-elseif (in_array($level, $senaraiPengguna)) 
+elseif (in_array($level, $senaraiPengguna))
 	$paras = '' . $level;
 else 
-	$paras = null;
+	$paras = null; # untuk pelawat sahaja
 
 echo "\r\r"; 
 
@@ -20,7 +20,7 @@ if ($paras == null): else: ?>
 <nav class="navbar navbar-inverse" role="navigation">
 <!-- div class="navbar navbar-custom" role="navigation" -->
 	<div class="container-fluid">
-		<!-- menu kiri mula -->
+<!-- menu kiri mula -->
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 			<span class="sr-only">Toggle navigation</span>
@@ -30,20 +30,18 @@ if ($paras == null): else: ?>
 			<a class="navbar-brand" href="<?php echo URL ?>"><?php echo Tajuk_Muka_Surat . ':' . $paras ?></a>
 			<a class="navbar-brand" href="<?php echo URL ?>ruangtamu/logout">
 				<span class="glyphicon glyphicon-off"></span>Keluar</a>
-			<a class="navbar-brand" href="<?php echo 'http://' . $_SERVER['SERVER_NAME'] ?>/private_html">
-				<span class="glyphicon glyphicon-stats"></span>E-Survey POM</a>	
 		</div>
-		<!-- menu kiri tamat -->
-		<!-- menu kanan mula -->
+<!-- menu kiri tamat -->
+<!-- menu kanan mula -->
 		<div class="navbar-collapse collapse">
 <?php require 'menubar_atas.php'; ?>
 			<!-- borang cari kp-->
 			<form class="navbar-form navbar-right" action="<?php echo URL ?>cari/idnama" method="POST">
 				<input type="submit" class="btn btn-info fa-input" value="&#xf002;">
-				<input type="text" name="cari" placeholder="Cari Newss / Nama" class="form-control">
+				<input type="text" name="cari" placeholder="Cari Nama" class="form-control">
 			</form>
 		</div>
-		<!-- menu kanan tamat -->
+<!-- menu kanan tamat -->
 	</div>
 </nav>
 <?php
