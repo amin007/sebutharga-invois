211f9b38046fd070e0b92a972b7cf627a4252232
amin007 <helmi007@yahoo.com>
2017-03-07 12:39:56 +0800

    tukar PROSESAN kepada KAKITANGAN, tukar 6/4 kepada 7

--- i-tatarajah.php
+++ i-tatarajah.php
@@ -15,16 +15,16 @@ define('Tajuk_Muka_Surat', '***');
 # setkan jquery, bootstrap dan font awesome sama ada local atau cdn
 ## cdn
       $jquery_cdn = 'https://code.jquery.com/jquery-2.2.3.min.js';
- $bootstrapJS_cdn = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js';
-$bootstrapCSS_cdn = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css';
- $ceruleanCSS_cdn = 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cerulean/bootstrap.min.css';
- $fontawesome_cdn = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css';
+ $bootstrapJS_cdn = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js';
+$bootstrapCSS_cdn = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css';
+ $ceruleanCSS_cdn = 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cerulean/bootstrap.min.css';
+ $fontawesome_cdn = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
 ## local
             $sumber = 'sumber/utama/';
       $jquery_local = $sumber . 'jquery/jquery-2.2.3.min.js';
- $bootstrapJS_local = $sumber . 'bootstrap/3.3.6/js/bootstrap.min.js';
-$bootstrapCSS_local = $sumber . 'bootstrap/3.3.6/css/bootstrap.min.css'; 
- $fontawesome_local = $sumber . 'font-awesome/4.4.0/css/font-awesome.min.css';
+ $bootstrapJS_local = $sumber . 'bootstrap/3.3.7/js/bootstrap.min.js';
+$bootstrapCSS_local = $sumber . 'bootstrap/3.3.7/css/bootstrap.min.css'; 
+ $fontawesome_local = $sumber . 'font-awesome/4.7.0/css/font-awesome.min.css';
 ############################################################################################
 ## isytihar konsan MYSQL dan GAMBAR ikut lokasi $server
 $ip = $_SERVER['REMOTE_ADDR'];
@@ -33,7 +33,7 @@ $server = $_SERVER['SERVER_NAME'];
 
 /*
 echo "<br>Alamat IP : <font color='red'>" . $ip . "</font> |
-\r<br>Nama PC : <font color='red'>" . $hostname . "</font> | 
+\r<br>Nama PC : <font color='red'>" . $hostname . "</font> |
 \r<br>Server : <font color='red'>" . $server . "</font>\r";
 //*/
 
@@ -68,7 +68,7 @@ else
 //echo DB_HOST . "," . DB_USER . "," . DB_PASS . ",," . DB_NAME . "<br>";
 ############################################################################################
 # buat tatasusunan ikut serialize
-define('PROSESAN', serialize( 
+define('KAKITANGAN', serialize( 
 	array ('abu','bakar','umar','osman','ali','hasan')
 	));
 ## data dalam database lain
