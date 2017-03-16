a7a3e96b8790f405b2ac0d8990c3d6517c95295c
amin007 <helmi007@yahoo.com>
2017-03-16 17:14:37 +0800

    tukar koding latarbelakarng dan buat 2 fungsi

--- sumber/fail/apps/teks-atas-gambar/borang2.php
+++ sumber/fail/apps/teks-atas-gambar/borang2.php
@@ -8,7 +8,7 @@ require($folder . 'tcpdf/textbox/textbox2.php');
 $pdf = new FPDI();
 $pdf->setSourceFile('contoh_borang.pdf');
 /*
-The 2nd paramenter – boxType, can take any one of the following values,
+The 2nd paramenter â€“ boxType, can take any one of the following values,
 
     /MediaBox
     /BleedBox
@@ -20,60 +20,67 @@ $tplIdx = $pdf->importPage(1, '/ArtBox');
 $pdf->addPage();
 $pdf->useTemplate($tplIdx, 0, 0, 0, 0, true); 
 
-# set warna latarbelakang
-$pdf->SetFont('Arial','',6);
-$pdf->SetTextColor(0, 0, 0);
-$pdf->SetFillColor(500,500,0);
+# set font
+$pdf->SetFont('Arial','',7);
 
 # nama syarikat
-$pdf->SetXY(51, 65.5); $pdf->Write(0,'AQIL DARWISY ENTERPRISE');
+$mainCon  = 'AQIL DARWISY ENTERPRISE';
+$mainCon2 = 'SPNB CONTRUCTION SDN BHD';
+$cellKeterangan = $mainCon . ' AS MAIN-CONTRACTOR ' .$mainCon2. ' AS PRINCIPAL';
+$pdf->SetFillColor(255,255,255); 
+# http://www.fpdf.org/en/doc/cell.htm
+# Cell(float w [, float h [, string txt [, mixed border [, int ln [, string align [, boolean fill [, mixed link]]]]]]])
+$pdf->SetXY(51.5, 64); $pdf->Cell(121.5, 3, $cellKeterangan ,0,0,'L',TRUE);
 
 # set pembolehubah
-$tarikh[] = '28-03-2017';
-$tarikh[] = '28-06-2018';
-$tarikh[] = '(I.E:10.01.2018)';
-$tarikh[] = '14-03-2017';
-//$no_inden = "NO. INDEN: PD10K/MLK/16/12-(04770)";
-$no_inden = 'NO. INDEN: ' . $_POST['data']['no_inden'];
-//$nama_pemohon = "NAMA PEMOHON: ROSLINDA BINTI ABU TALIB";
-$nama_pemohon = 'NAMA PEMOHON: ' . huruf('BESAR', $_POST['data']['nama_pemohon']);
-//$nric_no = "NRIC NO: 821113045546";
-$nric_no = 'NRIC NO: ' . huruf('BESAR', $_POST['data']['nric_no']);
-//$alamat_tapak[] = "ALAMAT TAPAK: NO. HAKMILK : HMM 1132 NI.LOT PT 2933";
-$alamat_tapak[] = 'ALAMAT TAPAK: NO. HAKMILK : ' . huruf('BESAR', $_POST['data']['alamat_tapak_1']);
-//$alamat_tapak[] = "MUKIM SUNGAI RAMBAI,  DAERAH JASIN MELAKA";
-$alamat_tapak[] = huruf('BESAR', $_POST['data']['alamat_tapak_2']);
-//$alamat_pemohon[] = "ALAMAT PEMOHON: NO.96, JALAN BR-3, TAMAN BUKIT RAMBAI ";
-$alamat_pemohon[] = 'ALAMAT PEMOHON: ' . huruf('BESAR', $_POST['data']['alamat_pemohon_1']);
-//$alamat_pemohon[] = "75200 ALOR GAJAH, MELAKA";
-$alamat_pemohon[] = huruf('BESAR', $_POST['data']['alamat_pemohon_2']);
+$data = setPembolehubah();
 
+$pdf->SetFont('Arial','',8);
 $paksiX = 100;
-$pdf->SetXY(59, $paksiX+1.5); $pdf->Write(0,$tarikh[0]);
-$pdf->SetXY(81, $paksiX+1.5); $pdf->Write(0,$tarikh[1]);
-$pdf->SetXY(51, 111); $pdf->Write(0,$tarikh[2]);
-$pdf->SetXY(51, 122); $pdf->Write(0,$tarikh[3]);
+$pdf->SetXY(59, $paksiX+1.5); $pdf->Write(0,$data[0]);
+$pdf->SetXY(81, $paksiX+1.5); $pdf->Write(0,$data[1]);
+$pdf->SetXY(51, 111); $pdf->Write(0,$data[2]);
+$pdf->SetXY(51, 122); $pdf->Write(0,$data[3]);
 
 $paksiX = 163;
-$pdf->SetXY(17, $paksiX+0); $pdf->Write(0,$no_inden);
-$pdf->SetXY(17, $paksiX+3); $pdf->Write(0,$nama_pemohon);
-$pdf->SetXY(17, $paksiX+6); $pdf->Write(0,$nric_no);
-$pdf->SetXY(17, $paksiX+9); $pdf->Write(0,$alamat_tapak[0]);
-$pdf->SetXY(40, $paksiX+12); $pdf->Write(0,$alamat_tapak[1]);
-$pdf->SetXY(17, $paksiX+15); $pdf->Write(0,$alamat_pemohon[0]);
-$pdf->SetXY(40, $paksiX+18); $pdf->Write(0,$alamat_pemohon[1]);
+$pdf->SetXY(17, $paksiX+0); $pdf->Write(0,$data[4]); //$no_inden);
+$pdf->SetXY(17, $paksiX+3); $pdf->Write(0,$data[5]); //$nama_pemohon);
+$pdf->SetXY(17, $paksiX+6); $pdf->Write(0,$data[6]); //$nric_no);
+$pdf->SetXY(17, $paksiX+9); $pdf->Write(0,$data[7]); //$alamat_tapak[0]);
+$pdf->SetXY(40, $paksiX+12); $pdf->Write(0,$data[8]); //$alamat_tapak[1]);
+$pdf->SetXY(17, $paksiX+15); $pdf->Write(0,$data[9]); //$alamat_pemohon[0]);
+$pdf->SetXY(40, $paksiX+18); $pdf->Write(0,$data[10]); //$alamat_pemohon[1]);
+
+
+/*# Begin with regular font
+$pdf->SetFont('Arial','',14);
+$pdf->Write(5,'Visit ');
+$pdf->SetTextColor(0,0,255); #  Then put a blue underlined link
+$pdf->SetFont('','U');
+$pdf->Write(5,'www.fpdf.org','http://www.fpdf.org');
 //*/
+
 $pdf->Output();
 
+function setPembolehubah() 
+{
+	$data[] = '28-03-2017';
+	$data[] = '28-06-2018';
+	$data[] = '(I.E:10.01.2018)';
+	$data[] = '14-03-2017';
+	//$no_inden = "NO. INDEN: PD10K/MLK/16/12-(04770)";
+	$data[] = 'NO. INDEN: ' . $_POST['data']['no_inden'];
+	$data[] = 'NAMA PEMOHON: ' . huruf('BESAR', $_POST['data']['nama_pemohon']);
+	$data[] = 'NRIC NO: ' . huruf('BESAR', $_POST['data']['nric_no']);
+	$data[] = 'ALAMAT TAPAK: NO. HAKMILK : ' . huruf('BESAR', $_POST['data']['alamat_tapak_1']);
+	$data[] = huruf('BESAR', $_POST['data']['alamat_tapak_2']);
+	$data[] = 'ALAMAT PEMOHON: ' . huruf('BESAR', $_POST['data']['alamat_pemohon_1']);
+	$data[] = huruf('BESAR', $_POST['data']['alamat_pemohon_2']);
+
+	return $data;
+}
 function huruf($jenis , $papar) 
 {
-	/*
-	$_POST=strtoupper($_POST['']['']);
-	$_POST=strtolower($_POST['']['']);
-	$_POST=mb_convert_case($_POST[''][''], MB_CASE_TITLE);
-	ucfirst
-	*/
-
 	switch ($jenis) 
 	{# mula - pilih $jenis
 	case "BESAR": # huruf('BESAR', )
