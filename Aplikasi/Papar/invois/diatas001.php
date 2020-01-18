6036fb123bf4f128b8f34122c2a87920f9caf4de
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-01-18 11:50:43 +0800

    tambah style

--- Aplikasi/Papar/invois/diatas001.php
+++ Aplikasi/Papar/invois/diatas001.php
@@ -9,5 +9,34 @@
 <title>Sebut Harga</title>
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+table.excel {
+	border-style:ridge;
+	border-width:1;
+	border-collapse:collapse;
+	font-family:sans-serif;
+	font-size:11px;
+}
+table.excel thead th, table.excel tbody th {
+	background:#CCCCCC;
+	border-style:ridge;
+	border-width:1;
+	text-align: center;
+	vertical-align: top;
+}
+table.excel tbody th { text-align:center; vertical-align: top; }
+table.excel tbody td { vertical-align:bottom; }
+table.excel tbody td
+{
+	padding: 0 3px; border: 1px solid #aaaaaa;
+	background:#ffffff;
+}
+.fa-input {font-family: FontAwesome}
+.teksBesar
+{
+	text-align:right;
+	font-size: 225%;
+}
+</style>
 </head>
 <body>
