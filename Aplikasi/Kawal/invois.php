596cc6ae27f3c905a6aa64b0ec4e6d31d7deac52
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2020-02-17 14:49:08 +0800

    ubah nilai pembolehubah

--- Aplikasi/Kawal/invois.php
+++ Aplikasi/Kawal/invois.php
@@ -37,7 +37,7 @@ class Invois extends \Aplikasi\Kitab\Kawal
 		$this->ulangJadual($senarai);
 	}
 #--------------------------------------------------------------------------------------------------
-	public function cthSebutHarga($a = 3500)
+	public function cthSebutHarga($a = 3500, $b = 100)
 	{
 		# isytihar pemboleubah
 		$this->papar->hargaProjek[] = $a;
@@ -45,9 +45,10 @@ class Invois extends \Aplikasi\Kitab\Kawal
 		$this->papar->carian = 'id';
 		$this->papar->syarikat = $this->tanya->contohDataSyarikat002();
 		$this->papar->akaun['kes'] = $this->tanya->contohSebutHarga002();
-		$this->papar->skop['s001'] = $this->tanya->jadualSkopProjek(WEB_APA);
-		$this->papar->jadual['j001'] = $this->tanya->contohJadual001();
-		$this->papar->jadual['j002'] = $this->tanya->contohJadual002();
+		$this->papar->skop['s001'] = $this->tanya->jadualSkopProjek(WEB_APA, $a);
+		$this->papar->skop['s002'] = $this->tanya->contohJadual000($b);
+		$this->papar->jadual['j001'] = $this->tanya->contohJadual001($b);
+		//$this->papar->jadual['j002'] = $this->tanya->contohJadual002($b);
 		//$this->debugDaa(); # semak data
 
 		# pergi papar kandungan
