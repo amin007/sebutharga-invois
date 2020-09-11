c57d737ba2c6138cd6f3588d94405020f6da2d2f
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-09-11 17:21:29 +0800

    contoh fail baru

---
+++ Aplikasi/Papar/invois/webKertasCadangan.php
@@ -0,0 +1,34 @@
+<?php
+# mula koding daa
+include 'diatas001.php';
+?>
+
+<div class="container">
+<div style="padding: 1rem 1rem;
+margin-bottom: 2rem;
+background-color: #e9ecef;
+border-radius: 0.3rem;">
+
+<?php
+#--------------------------------------------------------------------------------------------------
+?>
+	<ul>
+	<li>Pendahuluan dan objektif projek</li>
+	<li>Skop projek</li>
+	<li>Kerja-kerja yang terlibat</li>
+	<li>Proses kerja</li>
+	<li>Kerjasama (tanggungjawab) klien</li>
+	<li>Hakmilik dan hakcipta</li>
+	<li>Kos</li>
+	<li>Jadual dan kaedah bayaran</li>
+	<li>Profail syarikat dan portfolio</li>
+	</ul>
+<?php
+#--------------------------------------------------------------------------------------------------
+?>
+</div><!-- / class="jumbotron" -->
+</div><!-- / class="container" -->
+
+<?php
+include 'dibawah001.php';
+?>
