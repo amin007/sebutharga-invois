c2bb5c05ee378d7489b8877981b1eeb1380477c4
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2022-12-05 12:05:49 +0800

    jarakkan style 2 baris di bawah

--- Aplikasi/Papar/invois/diatas001.php
+++ Aplikasi/Papar/invois/diatas001.php
@@ -7,8 +7,13 @@
 <meta name="description" content="">
 <meta name="author" content="">
 <title>Sebut Harga</title>
+<?php
+echo $style . "\n\n";
+/*
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet" type="text/css">
+*/
+?>
 <style type="text/css">
 table.excel {
 	border-style:ridge;
